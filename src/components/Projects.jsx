23b8f44f777a123cde3f96d528e0c7adfd95145d
unknown <you@example.com>
2024-03-29 10:45:20 +0600

    Update Portfolio

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import ProjectItem from './ProjectItem'
+import ProjectItem from "./ProjectItem";
 
 const Projects = () => {
   return (
@@ -14,86 +13,86 @@ const Projects = () => {
       <div className="grid sm:grid-cols-2 gap-12">
         <ProjectItem
           img={
-            'https://img.freepik.com/free-psd/social-media-feed-template-companies-are-hiring_621600-56.jpg?size=626&ext=jpg&uid=R110489875&ga=GA1.2.1162881527.1690013272&semt=sph'
+            "https://img.freepik.com/free-psd/social-media-feed-template-companies-are-hiring_621600-56.jpg?size=626&ext=jpg&uid=R110489875&ga=GA1.2.1162881527.1690013272&semt=sph"
           }
           title="Mern-Job-Portal"
           code="https://github.com/baized1245/Ksc-job-portal"
-          web={'https://job-portal-d8491.web.app/'}
+          web={"https://job-portal-d8491.web.app/"}
         />
         <ProjectItem
           img={
-            'https://img.freepik.com/free-vector/online-library-isometric_98292-7026.jpg?size=626&ext=jpg&uid=R110489875&ga=GA1.1.1162881527.1690013272&semt=ais'
+            "https://img.freepik.com/free-vector/online-library-isometric_98292-7026.jpg?size=626&ext=jpg&uid=R110489875&ga=GA1.1.1162881527.1690013272&semt=ais"
           }
           title="Mern-Book-Store"
           code="https://github.com/baized1245/ksc-book-store"
-          web={'https://mern-book-store-1028f.web.app/'}
+          web={"https://mern-book-store-1028f.web.app/"}
         />
         <ProjectItem
           img={
-            'https://img.freepik.com/free-psd/delicious-burger-food-menu-social-media-banner-template_106176-1314.jpg?size=626&ext=jpg&uid=R110489875&ga=GA1.1.1162881527.1690013272&semt=sph'
+            "https://img.freepik.com/free-psd/delicious-burger-food-menu-social-media-banner-template_106176-1314.jpg?size=626&ext=jpg&uid=R110489875&ga=GA1.1.1162881527.1690013272&semt=sph"
           }
           title="Foodi"
           code="https://github.com/baized1245/Mern-foodi-client"
-          web={'https://boisterous-druid-a85a74.netlify.app/'}
+          web={"https://boisterous-druid-a85a74.netlify.app/"}
         />
         <ProjectItem
           img={
-            'https://img.freepik.com/free-vector/logistics-isometric-with-online-goods-delivery-service-workers-blue-3d-illustration_1284-28128.jpg?w=826&t=st=1703947758~exp=1703948358~hmac=d9dc3b8a112d1eaa9e9daab770e277a2ae454ea9fa8201b389a7b5453b50040e'
+            "https://img.freepik.com/free-vector/logistics-isometric-with-online-goods-delivery-service-workers-blue-3d-illustration_1284-28128.jpg?w=826&t=st=1703947758~exp=1703948358~hmac=d9dc3b8a112d1eaa9e9daab770e277a2ae454ea9fa8201b389a7b5453b50040e"
           }
           title="E-Commers App"
           code="https://github.com/baized1245/ema-john-simple-with-firebase-auth"
-          web={'https://ema-john-with-firebase-a-1fb59.web.app/'}
+          web={"https://ema-john-with-firebase-a-1fb59.web.app/"}
         />
         <ProjectItem
           img={
-            'https://img.freepik.com/free-vector/tablet-with-social-media-icons_1394-779.jpg?w=826&t=st=1703947990~exp=1703948590~hmac=5918c88193c820d391bcacb6fe8df9ef94bc58966e9167e7c46af2763301b562'
+            "https://img.freepik.com/free-vector/tablet-with-social-media-icons_1394-779.jpg?w=826&t=st=1703947990~exp=1703948590~hmac=5918c88193c820d391bcacb6fe8df9ef94bc58966e9167e7c46af2763301b562"
           }
           title="Disney 2.0"
           code="https://github.com/baized1245/Disney-2.0"
-          web={'https://wondrous-donut-13c087.netlify.app/'}
+          web={"https://wondrous-donut-13c087.netlify.app/"}
         />
         <ProjectItem
           img={
-            'https://img.freepik.com/free-vector/simple-monocolor-home-run-hero-baseball-t-shirt_742173-8050.jpg?size=626&ext=jpg&uid=R110489875&ga=GA1.1.1162881527.1690013272&semt=sph'
+            "https://img.freepik.com/free-vector/simple-monocolor-home-run-hero-baseball-t-shirt_742173-8050.jpg?size=626&ext=jpg&uid=R110489875&ga=GA1.1.1162881527.1690013272&semt=sph"
           }
           title="T-Shirt House"
           code="https://github.com/baized1245/Online-Cloth-Shop"
           web={
-            'https://659523c4a1c0ec7ad822a966--lively-unicorn-2f51d9.netlify.app/'
+            "https://659523c4a1c0ec7ad822a966--lively-unicorn-2f51d9.netlify.app/"
           }
         />
         <ProjectItem
           img={
-            'https://img.freepik.com/free-vector/back-school-landing-page_23-2148221369.jpg?size=626&ext=jpg&uid=R110489875&ga=GA1.1.1162881527.1690013272&semt=ais'
+            "https://img.freepik.com/free-vector/back-school-landing-page_23-2148221369.jpg?size=626&ext=jpg&uid=R110489875&ga=GA1.1.1162881527.1690013272&semt=ais"
           }
           title="Crash-Cours"
           code="https://github.com/baized1245/Landing-page-crash-cours"
           web={
-            'https://6598e3592f4c4e959ae1aa51--cozy-croissant-44035b.netlify.app/'
+            "https://6598e3592f4c4e959ae1aa51--cozy-croissant-44035b.netlify.app/"
           }
         />
         <ProjectItem
           img={
-            'https://img.freepik.com/free-psd/shoes-social-media-template-design_505751-4355.jpg?size=626&ext=jpg&uid=R110489875&ga=GA1.1.1162881527.1690013272&semt=sph'
+            "https://img.freepik.com/free-psd/shoes-social-media-template-design_505751-4355.jpg?size=626&ext=jpg&uid=R110489875&ga=GA1.1.1162881527.1690013272&semt=sph"
           }
           title="Nike"
           code="https://github.com/baized1245/Simple-nike-shoes-with-react-tailwind"
-          web={'https://earnest-sunshine-2f2a73.netlify.app/'}
+          web={"https://earnest-sunshine-2f2a73.netlify.app/"}
         />
         <ProjectItem
           img={
-            'https://img.freepik.com/premium-photo/modern-luxury-house-with-swimming-pool_691560-6289.jpg?size=626&ext=jpg&uid=R110489875&ga=GA1.1.1162881527.1690013272&semt=ais'
+            "https://img.freepik.com/premium-photo/modern-luxury-house-with-swimming-pool_691560-6289.jpg?size=626&ext=jpg&uid=R110489875&ga=GA1.1.1162881527.1690013272&semt=ais"
           }
           title="Air-CNC"
           code="https://github.com/baized1245/Air-CNC-clone"
           web={
-            'https://65b3caaa5a9191222774cf06--candid-daffodil-b2ad4e.netlify.app/'
+            "https://65b3caaa5a9191222774cf06--candid-daffodil-b2ad4e.netlify.app/"
           }
         />
         {/* <ProjectItem img={image4} title="Crypto app"/> */}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,18 +1,18 @@
-import React, { useState } from 'react'
+import { useState } from "react";
 import {
   AiOutlineHome,
   AiOutlineMenu,
   AiOutlineProject,
   AiOutlineMail,
-} from 'react-icons/ai'
-import { BsPerson } from 'react-icons/bs'
-import { GrProjects } from 'react-icons/gr'
+} from "react-icons/ai";
+import { IoDocumentText } from "react-icons/io5";
+import { GrProjects } from "react-icons/gr";
 
 const SideNav = () => {
-  const [nav, setNav] = useState(false)
+  const [nav, setNav] = useState(false);
   const handleNav = () => {
-    setNav(!nav)
-  }
+    setNav(!nav);
+  };
   return (
     <div>
       <AiOutlineMenu
@@ -31,14 +31,6 @@ const SideNav = () => {
           </a>
           <a
             onClick={handleNav}
-            href="#work"
-            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
-          >
-            <GrProjects size={20} />
-            <span className="pl-4">Work</span>
-          </a>
-          <a
-            onClick={handleNav}
             href="#projects"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
           >
@@ -47,10 +39,19 @@ const SideNav = () => {
           </a>
           <a
             onClick={handleNav}
-            href="#main"
+            href="#work"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
           >
-            <BsPerson size={20} />
+            <GrProjects size={20} />
+            <span className="pl-4">Project Details</span>
+          </a>
+
+          <a
+            onClick={handleNav}
+            href="https://drive.google.com/file/d/1tTo4hnrjOAe2oY93eSuDY6cKw9Nxi-L2/view"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
+          >
+            <IoDocumentText size={20} />
             <span className="pl-4">Resume</span>
           </a>
           <a
@@ -63,45 +64,51 @@ const SideNav = () => {
           </a>
         </div>
       ) : (
-        ''
+        ""
       )}
 
       <div className="md:block hidden fixed top-[25%] z-10">
         <div className="flex flex-col">
           <a
             href="#main"
-            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
+            className="flex items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
           >
             <AiOutlineHome size={20} />
-          </a>
-          <a
-            href="#work"
-            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
-          >
-            <GrProjects size={20} />
+            <span className="pl-4">Home</span>
           </a>
           <a
             href="#projects"
-            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
+            className="flex items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
           >
             <AiOutlineProject size={20} />
+            <span className="pl-4">Projects</span>
           </a>
           <a
-            href="#main"
-            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
+            href="#work"
+            className="flex items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
           >
-            <BsPerson size={20} />
+            <GrProjects size={20} />
+            <span className="pl-4">Project Details</span>
+          </a>
+
+          <a
+            href="https://drive.google.com/file/d/1tTo4hnrjOAe2oY93eSuDY6cKw9Nxi-L2/view"
+            className="flex items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
+          >
+            <IoDocumentText size={20} />
+            <span className="pl-4">Resume</span>
           </a>
           <a
             href="#contact"
-            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
+            className="flex items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
           >
             <AiOutlineMail size={20} />
+            <span className="pl-4">Contact</span>
           </a>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SideNav
+export default SideNav;
